456905a0e6e408e8a18e35e3f63a82373a905e60
b1acksmyth <smith.spence13@gmail.com>
2021-06-30 22:08:06 -0700

    setting up homepage structure

---
+++ src/app/containers/Homepage/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const PageContainer = styled.div`
+
+`;
+
+export function HomePage() {
+  return <PageContainer>
+    Hello World!
+  </PageContainer>
+}
