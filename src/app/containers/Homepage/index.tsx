ba7afe47ee95391bb747098b4e5432233098e133
b1acksmyth <smith.spence13@gmail.com>
2021-07-01 21:58:10 -0700

    add navbar and logo

--- src/app/containers/Homepage/index.tsx
+++ src/app/containers/Homepage/index.tsx
@@ -1,12 +1,23 @@
 import React from "react";
 import styled from "styled-components";
+import tw from "twin.macro";
+import { Navbar } from "../../components/NavBar";
 
 const PageContainer = styled.div`
-
+  ${tw`
+    flex
+    flex-col
+    w-full
+    h-full
+    items-center
+    overflow-x-hidden
+  `}
 `;
 
 export function HomePage() {
-  return <PageContainer>
-    Hello World!
-  </PageContainer>
+  return ( 
+    <PageContainer>
+      <Navbar />
+    </PageContainer>
+  );
 }
